5eab5582934c3ff5c9f8fc917fea75b82630e9e1
Joshua Samson <samsonjoshuagd@gmail.com>
2025-07-31 12:23:50 +0100

    Week One Task Files
    Reuploaded the week one files as a file for easy browsing

---
+++ Week1/joshuamain.js
@@ -0,0 +1,7 @@
+const bB = document.querySelector(".badButton");
+
+function bBClicked() {
+  alert("It works! I spent about 3 hours on this code. Mostly CSS!");
+}
+
+bB.addEventListener("click", bBClicked);
